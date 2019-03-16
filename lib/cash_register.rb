5104f318167635a0eafa2ff0691014f2c7305b10
Eyal Toledano <eyaltoledano@me.com>
2019-03-16 19:30:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,6 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total += (price * quantity)
     @quantity = quantity
-    binding.pry
     quantity.times do
       @@cart << title
     end
